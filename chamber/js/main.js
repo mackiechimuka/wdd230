d215cdd6d767a437eaa0e59294dd4c08db4b38db
mackiechimuka <mackiechimuka@gmail.com>
2022-02-05 21:57:18 +0200

    update

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,7 +1,40 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const date1 = new Date();
+const year = date1.getFullYear();
+document.getElementById("currentyear").textContent = year;
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+const date2 = document.querySelector("#currentdate");
 
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+try {
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  date2.textContent = new Date().toLocaleDateString("en-UK", options);
+} catch (e) {
+  alert("Error with code or your browser does not support Locale");
+}
+
+const now = new Date();
+const datehead =document.getElementById("time-today");
+const dateuk = new Intl.DateTimeFormat("en-UK", {  dateStyle: "full",}).format(now);
+datehead.innerHTML = `<em>${dateuk}</em>`;
+
+
+function toggleMenu() {
+  document.getElementById("navicon").classList.toggle("open");
+  document.getElementById("hum-button").classList.toggle("open");
+}
+
+const x = document.getElementById("hum-button");
+x.onclick = toggleMenu;
+
+window.onresize = () => {
+    if (window.innerWidth > 1022) {
+        navicon.classList.remove("open");
+        hum-button.classList.remove("open");
+    }
+  
+  }
+
